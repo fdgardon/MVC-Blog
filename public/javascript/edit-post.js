34057aac0e482ffb80913fa5993667d78113ea1a
fdgardon <119641606+fdgardon@users.noreply.github.com>
2023-04-28 16:19:57 -0700

    delete functions fixed

--- public/javascript/edit-post.js
+++ public/javascript/edit-post.js
@@ -1,28 +1,27 @@
-async function editFormHandler(event) {
+const postId = document.querySelector('input[name="post-id"]').value;
+
+const editFormHandler = async function(event) {
   event.preventDefault();
 
   const title = document.querySelector('input[name="post-title"]').value;
-  const post_text = document.querySelector('textarea[name="post-text"]').value;
-  const id = window.location.toString().split('/')[
-    window.location.toString().split('/').length-1
-  ];
+  const body = document.querySelector('textarea[name="post-text"]').value;
 
-  const response = await fetch(`/api/posts/${id}`, {
+  await fetch(`/api/post/${postId}`, {
     method: 'PUT',
     body: JSON.stringify({
       title,
-      post_text
+      body
     }),
     headers: {
       'Content-Type': 'application/json'
     }
   });
 
-  if (response.ok) {
-    document.location.replace('/dashboard/');
-  } else {
-    alert(response.statusText);
-  }
-}
+  document.location.replace('/dashboard');
+};
 
-document.querySelector('.edit-post-form').addEventListener('submit', editFormHandler);
+document
+  .querySelector('#edit-post-form')
+  .addEventListener('submit', editFormHandler);
+
+  document.querySelector('.edit-post-form').addEventListener('submit', editFormHandler);
