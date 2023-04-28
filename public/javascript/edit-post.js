2a4d42dc8eb2746f37c41c38edc00d26d1728f0b
fdgardon <119641606+fdgardon@users.noreply.github.com>
2023-04-28 16:47:11 -0700

    save post is fixed

--- public/javascript/edit-post.js
+++ public/javascript/edit-post.js
@@ -1,27 +1,56 @@
-const postId = document.querySelector('input[name="post-id"]').value;
+// const postId = document.querySelector('input[name="post-id"]').value;
 
-const editFormHandler = async function(event) {
+// const editFormHandler = async function(event) {
+//   event.preventDefault();
+
+//   const title = document.querySelector('input[name="post-title"]').value;
+//   const body = document.querySelector('textarea[name="post-text"]').value;
+
+//   await fetch(`/api/post/${postId}`, {
+//     method: 'PUT',
+//     body: JSON.stringify({
+//       title,
+//       body
+//     }),
+//     headers: {
+//       'Content-Type': 'application/json'
+//     }
+//   });
+
+//   document.location.replace('/dashboard');
+// };
+
+// document
+//   .querySelector('#edit-post-form')
+//   // .addEventListener('submit', editFormHandler);
+
+//   document.querySelector('.edit-post-form').addEventListener('submit', editFormHandler);
+
+async function editFormHandler(event) {
   event.preventDefault();
 
   const title = document.querySelector('input[name="post-title"]').value;
-  const body = document.querySelector('textarea[name="post-text"]').value;
+  const post_text = document.querySelector('textarea[name="post-text"]').value;
+  const id = window.location.toString().split('/')[
+    window.location.toString().split('/').length-1
+  ];
 
-  await fetch(`/api/post/${postId}`, {
+  const response = await fetch(`/api/posts/${id}`, {
     method: 'PUT',
     body: JSON.stringify({
       title,
-      body
+      post_text
     }),
     headers: {
       'Content-Type': 'application/json'
     }
   });
 
-  document.location.replace('/dashboard');
-};
+  if (response.ok) {
+    document.location.replace('/dashboard/');
+  } else {
+    alert(response.statusText);
+  }
+}
 
-document
-  .querySelector('#edit-post-form')
-  // .addEventListener('submit', editFormHandler);
-
-  document.querySelector('.edit-post-form').addEventListener('submit', editFormHandler);
+document.querySelector('.edit-post-form').addEventListener('submit', editFormHandler);
